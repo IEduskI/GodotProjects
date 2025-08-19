940e418f8c589e0128de8db590104edeed6a0827
Edusk <eduardo.campos.am@gmail.com>
2025-08-18 20:41:56 -0600

    Add Tappy project with plane and pipes logic

---
+++ Tappy/Scenes/Plane/plane.gd
@@ -0,0 +1,33 @@
+extends CharacterBody2D
+
+const JUMP_POWER: float = -350.0
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+var _gravity: float = ProjectSettings.get("physics/2d/default_gravity")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _physics_process(delta: float) -> void:
+	fly(delta)
+	move_and_slide()
+	if is_on_floor():
+		die()
+
+func fly(delta: float) -> void:
+	velocity.y += _gravity * delta
+	if Input.is_action_just_pressed("jump") == true:
+		velocity.y = JUMP_POWER
+		animation_player.play("jump")
+
+func die() -> void:
+	animated_sprite_2d.stop()
+	set_physics_process(false)
