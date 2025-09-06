790bb735b731211c9de8ec7fcb95a01fc142b8d3
Edusk <eduardo.campos.am@gmail.com>
2025-09-05 22:00:35 -0600

    Update main scene

---
+++ Tappy/Scenes/Main/main.gd
@@ -0,0 +1,12 @@
+extends Control
+
+const GAME = preload("res://Scenes/Game/Game.tscn")
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("jump"):
+		print("_unhandled_input:", event)
+		get_tree().change_scene_to_packed(GAME)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
